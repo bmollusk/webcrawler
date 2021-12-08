5530d39929822ec1d94bbe0e703c83b3ca826c91
jacktag6 <jacksonhunt@ufl.edu>
2021-12-07 23:36:46 -0500

    much quicker

--- bfs.py
+++ bfs.py
@@ -14,9 +14,10 @@ def bfs(source, size):
         q.pop(0)
         neighbors = gahl.get_all_links(u)
         for v in neighbors:
-            if len(q) + G.number_of_nodes() <= size:
-                if v not in visited:
-                    q.append(v)
-                    visited.add(v)
-                G.add_edge(u, v)
+            if G.number_of_nodes() == size:
+                break
+            if v not in visited:
+                q.append(v)
+                visited.add(v)
+            G.add_edge(u, v)
     return G
