7e8cd841a23086820711bbd10ab7a0d239f83ada
bmollusk <rateebriyasat@gmail.com>
2021-12-07 23:43:27 -0500

    main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import bfs
 import networkx as nx
 import matplotlib.pyplot as plt
-originURL = input("Enter URL of origin: ")
-#https://en.wikipedia.org/wiki/MissingNo.
-size = int(input("Enter size of graph: "))
-G = bfs.bfs(originURL, size)
-print(G)
-subax1=plt.subplot(121)
-nx.draw(G, with_labels=True)
-plt.show()
+if __name__ == '__main__':
+    originURL = input("Enter URL of origin: ")
+    #https://en.wikipedia.org/wiki/MissingNo.
+    size = int(input("Enter size of graph: "))
+    G = bfs.bfs(originURL, size)
+    print(G)
+    subax1=plt.subplot(121)
+    nx.draw(G, with_labels=True)
+    plt.show()
